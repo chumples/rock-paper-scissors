e931bc2eaf1bc5cef217b15bdc2baacdc6c7746b
chumples <84584789+chumples@users.noreply.github.com>
2021-06-15 11:51:09 +0700

    palindrome solutions

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+// //Free Code Camp's solution
 
-};
+function palindromes(str) {
+    const alphanumericOnly = str
+        .toLowerCase() // lowercase everything
+        .match(/[a-z0-9]/g); // strip out non-alphanumeric characters
+
+    return alphanumericOnly.join('') ===
+        alphanumericOnly.reverse().join('');
+}
+
+// ODIN's solution
+// const palindromes = function(string) {
+//     processedString = string.toLowerCase().replace(/[^A-Za-z]/g, '');
+//     return (
+//         processedString
+//         .split('')
+//         .reverse()
+//         .join('') === processedString
+//     );
+// };
 
 module.exports = palindromes;
